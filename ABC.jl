b1f4d05df9a35bf4023baeb0df21c72fc67f3fb6
Oleksiy <oleksiy.smola@gmail.com>
2025-07-19 14:54:08 +0100

    prints output

--- ABC.jl
+++ ABC.jl
@@ -13,6 +13,12 @@ function convertAnglesToRadians(structuralParameters::Vector{Float64})::Vector{F
     return structuralParameters
 end
 
+function convertAnglesToDegrees(structuralParameters::Vector{Float64})::Vector{Float64}
+    convertToRadians::Float64 = pi/180.0
+    structuralParameters[3] = structuralParameters[3]/convertToRadians
+    return structuralParameters
+end
+
 function defineXiCoordinates(internalCoordinates::Vector{Float64}, structuralParameters::Vector{Float64})::Vector{Float64}
     convertToRadians::Float64 = pi/180
     numberOfModes::Int64 = length(internalCoordinates)
@@ -27,7 +33,7 @@ function defineXiCoordinates(internalCoordinates::Vector{Float64}, structuralPar
     xi[1:numberOfStretches] = 1 .- exp.(-morseParameters.*stretchDisplacement)
     # xi[1:numberOfStretches] = stretchDisplacement
 
-    # xi[numberOfStretches+1:end] = (internalCoordinates[numberOfStretches+1:end] - equilibriumParameters[numberOfStretches+1:end]).*convertToRadians
+    # xi[numberOfStretches+1:end] = internalCoordinates[numberOfStretches+1:end] - equilibriumParameters[numberOfStretches+1:end]
     # # Trigonometric-type function for the bending 
     xi[numberOfStretches+1:end] = cos.(equilibriumParameters[numberOfStretches+1:end]) .- cos.(internalCoordinates[numberOfStretches+1:end])
     # xi[numberOfStretches+1:end] = cos.(pi .- internalCoordinates[numberOfStretches+1:end].*convertToRadians) .- cos.(pi .- equilibriumParameters[numberOfStretches+1:end].*convertToRadians)
