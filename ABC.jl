0ba26b191631ca2757b47a77169d537209769a79
alex smola <asmola@vincent.theory.phys.ucl.ac.uk>
2025-07-20 17:09:54 +0100

    Forward difference for non-linear part still needs work

--- ABC.jl
+++ ABC.jl
@@ -19,23 +19,24 @@ function convertAnglesToDegrees(structuralParameters::Vector{Float64})::Vector{F
     return structuralParameters
 end
 
-function defineXiCoordinates(internalCoordinates::Vector{Float64}, structuralParameters::Vector{Float64})::Vector{Float64}
+function defineXiCoordinates(internalCoordinates::Vector{Float64}, structuralParameters)
+    T::DataType = eltype(structuralParameters)
     convertToRadians::Float64 = pi/180
     numberOfModes::Int64 = length(internalCoordinates)
     numberOfAtoms::Int64 = (numberOfModes + 6)/3
     numberOfStretches::Int64 = numberOfAtoms - 1
-    xi::Vector{Float64} = zeros(numberOfModes)
-    equilibriumParameters::Vector{Float64} = structuralParameters[1:numberOfModes]
-    morseParameters::Vector{Float64} = structuralParameters[numberOfModes+1:end]
+    xi::Vector{T} = zeros(numberOfModes)
+    equilibriumParameters = structuralParameters[1:numberOfModes]
+    morseParameters = structuralParameters[numberOfModes+1:end]
     
     # Define Morse-like coordinates for stretches
-    stretchDisplacement::Vector{Float64} = internalCoordinates[1:numberOfStretches] - equilibriumParameters[1:numberOfStretches]
+    stretchDisplacement = internalCoordinates[1:numberOfStretches] - equilibriumParameters[1:numberOfStretches]
     xi[1:numberOfStretches] = 1 .- exp.(-morseParameters.*stretchDisplacement)
     # xi[1:numberOfStretches] = stretchDisplacement
 
     # xi[numberOfStretches+1:end] = internalCoordinates[numberOfStretches+1:end] - equilibriumParameters[numberOfStretches+1:end]
     # # Trigonometric-type function for the bending 
-    xi[numberOfStretches+1:end] = cos.(equilibriumParameters[numberOfStretches+1:end]) .- cos.(internalCoordinates[numberOfStretches+1:end])
+    xi[numberOfStretches+1:end] = cos.(internalCoordinates[numberOfStretches+1:end]) .- cos.(equilibriumParameters[numberOfStretches+1:end])
     # xi[numberOfStretches+1:end] = cos.(pi .- internalCoordinates[numberOfStretches+1:end].*convertToRadians) .- cos.(pi .- equilibriumParameters[numberOfStretches+1:end].*convertToRadians)
     # xi[numberOfStretches+1:end] = sin.(pi .- internalCoordinates[numberOfStretches+1:end].*convertToRadians) .- cos.(pi .- equilibriumParameters[numberOfStretches+1:end].*convertToRadians)
     return xi
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## Julia fitting for multi-dimensional potential energy surfaces
 My attempt (perhaps poorly) to construct a fitter for a potential energy surface. The fundamental premise is broken into two steps. The current implementation uses stochastic mini-batch gradient descent.
+
+Barrier to linearity ~ 17436 cm-1
